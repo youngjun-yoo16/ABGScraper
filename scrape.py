9083aed2bbb3ca48dd0a4d499d53bdbba79258d2
youngjun-yoo16 <yoo156@purdue.edu>
2022-05-26 14:22:34 +0900

    Experimentation with instaloader library

--- scrape.py
+++ scrape.py
@@ -1,15 +1,6 @@
-from pprint import pprint
-import instagram_scraper
+import instaloader
 
 if __name__ == '__main__':
-    args = {"login_user": "test.account0916", "login_pass": "dbdudwns"}
-    insta_scraper = instagram_scraper.InstagramScraper(**args)
-    insta_scraper.authenticate_with_login()
-    shared_data = insta_scraper.get_shared_data_userinfo(username='thefreshmanclub_purdue')
-    
-    arr = []
-    
-    for item in insta_scraper.query_media_gen(shared_data):
-        arr.append(item)
-
-    pprint(arr)
+    bot = instaloader.Instaloader()
+    username = input("Enter the username of the account which you want to scrap: ")
+    bot.download_profile(username, profile_pic_only = True)
