62db14b12308b837001aa5ac7a235bcab0c60a35
youngjun-yoo16 <yoo156@purdue.edu>
2022-05-26 13:58:02 +0900

    Experimenting with instagram scraper

--- scrape.py
+++ scrape.py
@@ -1,19 +1,15 @@
-from instagramy import InstagramUser
+from pprint import pprint
+import instagram_scraper
 
 if __name__ == '__main__':
-    unInstance = input("username: ")
-    user = InstagramUser(unInstance) 
-    # printing the basic details like 
-    # followers, following, bio 
-    print(user.is_verified()) 
-    print(user.popularity()) 
-    print(user.get_biography()) 
+    args = {"login_user": "test.account0916", "login_pass": "dbdudwns"}
+    insta_scraper = instagram_scraper.InstagramScraper(**args)
+    insta_scraper.authenticate_with_login()
+    shared_data = insta_scraper.get_shared_data_userinfo(username='thefreshmanclub_purdue')
+    
+    arr = []
+    
+    for item in insta_scraper.query_media_gen(shared_data):
+        arr.append(item)
 
-    # return list of dicts 
-    posts = user.get_posts_details() 
-
-    print('\n\nLikes', 'Comments') 
-    for post in posts: 
-        likes = post["likes"] 
-        comments = post["comment"] 
-        print(likes,comments)
+    pprint(arr)
