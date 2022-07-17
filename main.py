33389d55282de9693b1f42a46529ac17a16baceb
youngjun-yoo16 <yoo156@purdue.edu>
2022-07-17 21:57:48 +0900

    Used jinja and fastapi

---
+++ main.py
@@ -0,0 +1,13 @@
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
