bfcf108afacecac3160de89ed08787ca717bac33
youngjun-yoo16 <yoo156@purdue.edu>
2022-07-18 00:22:33 +0900

    Experimentation with the post method of FastApi

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import uvicorn
-from fastapi import FastAPI, Request
+import instaloader
+from fastapi import FastAPI, Request, Form
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
@@ -7,7 +7,20 @@ from fastapi.responses import HTMLResponse
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
+bot = instaloader.Instaloader()
 
-@app.get("/", response_class=HTMLResponse)
-def home(request: Request):
+@app.get("/")
+async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+#FastApi treats the parameter of the post method as that of flask's request.form[name] 
+#In this case, operations is the same as request.form["operations"]
+@app.post("/")
+async def get_username(operations: str = Form()):
+    print(operations)
+    
+
+@app.get("/pfp")
+async def download_users_profile_picture(username, request: Request):
+    bot.download_profile(username, profile_pic_only = True)
+    return templates.TemplateResponse("pfp.html", {"request": request})
